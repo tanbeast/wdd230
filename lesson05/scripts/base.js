e15b6c2f5f7e77e73adda4b44ec097f358e2cd43
Kade O'Crowley <tanbeast3@gmail.com>
2024-02-08 08:17:25 -0700

    .

--- lesson05/scripts/base.js
+++ lesson05/scripts/base.js
@@ -1,21 +1,32 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document .querySelector("#list");
 
-const input = document.querySelector("#favchap")
-const button = document.querySelector("button")
-const list = document.querySelector("#list")
-button.addEventListener("click", () => {
-     // Check if the input is not blank
+button.addEventListener("click", ()=>{
     if (input.value != ""){
-        const li = document.createElement('li');
-        const deleteButton = document.createElement('button');
-        li.textContent = input.value;
-        deleteButton.textContent = '❌';
-        li.append(deleteButton);
-        list.append(li);
-        deleteButton.addEventListener('click', function() {
-            list.removeChild(li);
-            input.focus()
-        });
-        input.focus()
+    const myChap = input.value;
+    
+
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    li.textContent = myChap;
+    deleteButton.textContent = "❌";
+    deleteButton.setAttribute("aria-label","delete button")
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener("click", ()=>{
+        li.remove();
+        input.focus();
         input.value = "";
-    }
-})
+    })
+
+
+    input.focus();
+
+
+
+
+
+}
+}
+)
