28aa25080783fa696b1f00631aac3c8542aeffa9
Kade O'Crowley <tanbeast3@gmail.com>
2024-02-14 14:57:50 -0700

    first test for visits

---
+++ scripts/vistcount.js
@@ -0,0 +1,6 @@
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+visitsDisplay.textContent = numVisits;
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
