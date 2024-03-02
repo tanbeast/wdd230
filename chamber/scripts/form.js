cfecf91566aaa62775b1cff0ac229b584fe69370
Kade O'Crowley <tanbeast3@gmail.com>
2024-03-02 12:01:07 -0700

    first test for join page

---
+++ chamber/scripts/form.js
@@ -0,0 +1,5 @@
+// Get the current timestamp in milliseconds
+const currentTimestamp = Date.now();
+
+// Set the value of the hidden input field
+document.getElementById("timestamp").value = currentTimestamp;
