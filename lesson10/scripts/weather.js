63d287395f0da7f634046d8469cda174cdfe8ec2
Kade O'Crowley <tanbeast3@gmail.com>
2024-03-11 22:56:22 -0600

    .

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -19,7 +19,9 @@ async function apiFetch() {
       
       // Output the results to the console for testing
       console.log(data);
-      displayResults(data);
+      
+      // Manipulate HTML elements to display weather information
+      
     } else {
       // If response is not OK, throw an error
       throw Error(await response.text());
@@ -31,6 +33,9 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
+  currentTemp.textContent = data.main.temp + ' °F';
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  captionDesc.textContent = data.weather[0].description;
   // Capitalize each word of the weather description
   const capitalizedDesc = data.weather[0].description.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
   captionDesc.textContent = capitalizedDesc;
@@ -44,4 +49,4 @@ function displayResults(data) {
 
 // Invoke the apiFetch function
 apiFetch();
-
+displayResults();
