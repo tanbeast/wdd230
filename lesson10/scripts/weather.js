0e0fb7c4632b95885c11266ef004b1beebed6ca9
Kade O'Crowley <tanbeast3@gmail.com>
2024-03-11 22:17:35 -0600

    .

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -34,5 +34,15 @@ async function apiFetch() {
   }
 }
 
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+
 // Invoke the apiFetch function
 apiFetch();
